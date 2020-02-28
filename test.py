9b106acad4214a2ddab6d15a15dc0e839bb933c7
Dan-Eli <bergeronpilon@gmail.com>
2020-02-28 08:27:08 -0500

    Add test file

---
+++ test.py
@@ -0,0 +1,21 @@
+from shapely.geometry import Point, LineString
+from shapely.strtree import STRtree
+
+lines = []
+for i in range(10):
+    line = LineString([(0,0),(i,i)])
+    line.__att = i+4
+    lines.append(line)
+
+tree = STRtree(lines)
+lines = None
+box = tree.query(Point(4,4).buffer(0.99))
+
+print (len(box))
+for line in box:
+    print (line.__att)
+    line._att1 = 27
+
+
+print ('fin')
+
